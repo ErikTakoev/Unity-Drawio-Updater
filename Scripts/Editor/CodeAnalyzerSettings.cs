65892132697348ea0309d3f6d1d2f855d9804cc9
Erik Takoev <eriktakoev@gmail.com>
2025-05-26 03:06:24 +0300

    Added tooltips for namespace filters and output directory in CodeAnalyzerSettings to improve usability and clarity.

--- Scripts/Editor/CodeAnalyzerSettings.cs
+++ Scripts/Editor/CodeAnalyzerSettings.cs
@@ -5,7 +5,10 @@ namespace Expecto
     [CreateAssetMenu(fileName = "CodeAnalyzerSettings", menuName = "Expecto/CodeAnalyzerSettings")]
     public class CodeAnalyzerSettings : ScriptableObject
     {
+        [Tooltip("CodeAnalyzer will analyze only classes from these namespaces")]
         public string[] namespaceFilters;
+
+        [Tooltip("Output directory for generated XML files")]
         public string outputDirectory;
     }
 }
