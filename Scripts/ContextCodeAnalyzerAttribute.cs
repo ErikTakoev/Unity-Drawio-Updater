d5de1ce6c03b9aae1083332942791aa654d3fc0d
Erik Takoev <eriktakoev@gmail.com>
2025-05-27 14:03:51 +0300

    Update .gitignore to exclude __pycache__ meta files and enhance ContextCodeAnalyzerAttribute to support property attributes.

--- Scripts/ContextCodeAnalyzerAttribute.cs
+++ Scripts/ContextCodeAnalyzerAttribute.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Expecto
 {
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Field)]
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Field | AttributeTargets.Property)]
     public class ContextCodeAnalyzerAttribute : Attribute
     {
         public string Context { get; }
