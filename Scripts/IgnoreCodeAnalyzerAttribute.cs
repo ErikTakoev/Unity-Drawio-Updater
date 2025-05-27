4b43337805b93081e54cba3fa8292bca53fad374
Erik Takoev <eriktakoev@gmail.com>
2025-05-27 14:51:52 +0300

    Remove CodeAnalyzer

--- Scripts/IgnoreCodeAnalyzerAttribute.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Expecto
-{
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Enum | AttributeTargets.Method | AttributeTargets.Field)]
-    public class IgnoreCodeAnalyzerAttribute : Attribute
-    {
-    }
-}
